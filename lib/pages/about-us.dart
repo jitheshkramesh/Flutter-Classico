db42f63bfd170cb42a0943976251788a000deaf0
jitheshkr <87263767+jitheshkr@users.noreply.github.com>
2023-07-04 16:58:04 +0530

    location selection

---
+++ lib/pages/about-us.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Aboutus extends StatelessWidget {
+  const Aboutus({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('About us page'),
+    );
+  }
+}
